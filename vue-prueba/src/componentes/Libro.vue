596961fa708d9d0efe3dbf1973c9fcdc15f4c03c
GiseMa <gisellemaravankin7@hotmail.com>
2024-06-03 17:58:13 -0300

    Reagregue las dependencias

---
+++ vue-prueba/src/componentes/Libro.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+
+  <section class="vue-prueba-src-componentes-libro">
+    <h1>Formulario de alta de libros</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'vue-prueba-src-componentes-libro',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+  .vue-prueba-src-componentes-libro {
+
+  }
+</style>
