71fc26e7d6c8698bf67cb3566a3d301f0473ba58
francoo177 <franco.rios@bue.edu.ar>
2024-06-08 09:40:31 -0300

    escribi un comentario

--- vue-prueba/src/servicios/libros/main.js
+++ vue-prueba/src/servicios/libros/main.js
@@ -6,3 +6,6 @@ import { deleteLibro } from './deleteLibro';
 
 //Aca pensaba podriamos probar las funciones para chequear que onda o ponerlas en plan
 // mas detalladas 
+// Podriamos usar AXIOS en vez de hacer Fetch.
+// Estuve viendo unos CRUD con Vue y en realidad la logica de las llamadas a las API se hace en los
+// archivos .vue, no en solo un .js
