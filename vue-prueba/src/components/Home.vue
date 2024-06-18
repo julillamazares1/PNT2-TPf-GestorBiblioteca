04c28ddc3be256159f7a2cd0d773d24bdba7ff13
GiseMa <gisellemaravankin7@hotmail.com>
2024-06-18 19:07:57 -0300

    Borre comentarios y modifique el carrusel

--- vue-prueba/src/components/Home.vue
+++ vue-prueba/src/components/Home.vue
@@ -1,49 +1,3 @@
-<!-- 
- 
-CODIGO ORIGINAL DESCARGADO DEL REPO DE GIT
-
-
-<template>
-  <section id="home">
-    <h3>Te invitamos a conocer nuestra colección de libros y que encuentres tus próximos favoritos.</h3>
-  </section>
-
-  <div id="carouselExample" class="carousel slide">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="../assets/rayuelaFrase.jpg" class="d-block w-80" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="../assets/bestiario.jpg" class="d-block w-80" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="../assets/cronicamuerte2.png" class="d-block w-90" alt="...">
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'Home',
-};
-</script>
-
-<style scoped>
-/* Estilos específicos para la página de inicio */
-</style> -->
-
-
-
-<!-- CORRECCION  -->
 
 <template>
   <div id="home-container">
@@ -82,21 +36,23 @@ export default {
 </script>
 
 <style scoped>
-/* Estilos específicos para la página de inicio */
-#home-container {
+.carousel-inner {
   width: 100%;
+  overflow: hidden;
 }
-.carousel-inner img {
-  width: 100%;
-  height: auto;
+
+.carousel-item {
+  text-align: center;
+}
+
+.carousel-item img {
+  margin: 0 auto;
+  display: block;
 }
 
 .carousel-control-prev,
 .carousel-control-next {
-  color: white;
-}
-.carousel-control-prev:hover,
-.carousel-control-next:hover {
-  color: black; 
+  background-color: rgb(171, 199, 157);
+  color: rgb(65, 109, 25); 
 }
 </style>
